dc105983e2d84bbc02c93e3947ce2700114b9eea
Pedro Domingues <pedrodominsan@gmail.com>
2025-08-15 16:26:15 -0300

    Inclusão de um carrossel perfeito
    Foi feita a inclusão de um carrossel das categorias da seção 2. Os outros 2 serão feitos a partir do primeiro. Então está relativamente fácil.

--- Curso-Matheus-B/Market.js
+++ Curso-Matheus-B/Market.js
@@ -1,5 +1,25 @@
-function voltar() {
-    document.getElementById('btn-back')
 
-    
+contador = 0
+
+function avancar() {
+    let carrossel = document.getElementById('carrossel-brinquedos')
+
+    if (carrossel.style.left != "-200%") 
+    {
+        contador += -25;
+    }
+
+    carrossel.style.left = `${contador}%`;
 }
+function voltar() {
+    let carrossel = document.getElementById('carrossel-brinquedos')
+    let posAtual = getComputedStyle(carrossel).left;
+
+    if (posAtual !== "0px" && carrossel.style.left != '0%') 
+    {
+        contador += 25;
+    }
+
+    carrossel.style.left = `${contador}%`;
+}
+
