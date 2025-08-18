a52d3380f96e32adf73274a8fe8bade5e5958164
Pedro Domingues <pedrodominsan@gmail.com>
2025-08-18 16:52:39 -0300

    Implementação de modais
    Foram feitas implementações de modais a fim de realizar validações e também seleções com interatividade com o usuário. Dessa forma, faremos com que a página seja fechada para erros.

--- Curso-Matheus-B/Market.js
+++ Curso-Matheus-B/Market.js
@@ -23,3 +23,55 @@ function voltar() {
     carrossel.style.left = `${contador}%`;
 }
 
+function modal_avancar() {
+    const opcoes = document.querySelectorAll('.checkbox')
+    let modal_start = document.getElementById('modal-select')
+    let modal_erro = document.getElementById('modal-erro')
+    let fundo_escuro = document.getElementById('fundo-escuro')
+    let selecionados = [];
+
+    opcoes.forEach(function(opcao) {
+        if (opcao.checked) {
+            selecionados.push(opcao.value);
+        }
+    });
+
+    if (selecionados.length != 3) {
+        modal_erro.style.display = 'flex'
+        modal_erro.style.flexDirection = 'column'
+    }
+        else {
+        modal_start.close()
+        fundo_escuro.style.display = 'none'
+    } 
+
+    for (let i = 0; i < 1; i++) {
+        if (selecionados[i].value === 'brinquedos') { 
+            let titulo_cat = document.getElementById('title-cat')
+
+            titulo_cat.innerHTML('Brinquedos')
+        }
+        if (selecionados[i].value == 'eletrodomesticos') { 
+
+        }
+        if (selecionados[i].value == 'jardinagem') { 
+            
+        }
+        if (selecionados[i].value == 'roupaseacessorios') { 
+
+        }
+        if (selecionados[i].value == 'tecnologia') { 
+
+        }
+        if (selecionados[i].value == 'utensiliosdecozinha') { 
+
+        }
+    }
+}
+
+function fechar_modal_erro() {
+    let modal_erro = document.getElementById('modal-erro')
+
+    modal_erro.style.display = 'none'
+}
+
