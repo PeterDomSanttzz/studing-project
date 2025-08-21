f7a5bf1add0502953458738d178c69d56ee1eacb
Pedro Domingues <pedrodominsan@gmail.com>
2025-08-21 17:25:15 -0300

    Adição de arquivos novos para estudos novos
    Aqui foram feitas 3 coisas:
    
    1. Adição de arquivos HTML, CSS e JS
    
    2. Preenchimento de elementos dinamicamente com JS.
    
    3. Leitura de URL->JSON->Dados. Ou seja, consumo de API.
    
    Com relação à lógica de arrows function, necessário aprimorar conhecimentos.

---
+++ Estudos-JS-JQuery/script.js
@@ -0,0 +1,35 @@
+const URL = 'https://dummyjson.com/products';
+
+function exec() {
+    let resp_usuario = document.getElementById('num-produto');
+    let img = document.getElementById('img');
+    let nome = document.getElementById('name');
+    let preco = document.getElementById('preco');
+    let error_message = document.getElementById('error-message');
+
+    resp_usuario.value = resp_usuario.value - 1;
+    error_message.style.display = 'none'
+
+    if (resp_usuario.value < 0 || resp_usuario.value > 29) {
+        error_message.style.display = 'flex'
+        error_message.innerText = 'Produto não encontrado!'
+
+        nome.style.display = 'none'
+        preco.style.display = 'none'
+        img.style.display = 'none'
+    }
+    else {
+        nome.style.display = 'flex'
+        preco.style.display = 'flex'
+        img.style.display = 'flex'
+
+        fetch(URL)
+        .then(res => res.json())
+        .then(dados => {
+            const produto = dados.products[resp_usuario.value]
+            nome.innerText = produto.title
+            preco.innerText = 'R$' + produto.price
+            img.src = produto.images[0]
+        });
+    }
+}
